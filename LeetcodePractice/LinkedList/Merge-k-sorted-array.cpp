c71534975515330affc5e0162850a9c6b1f05f7a
Mrunmay <mrunmayurkude20@gmail.com>
2026-05-19 14:47:27 +0530

    added merge k sorted array

--- LeetcodePractice/LinkedList/Merge-k-sorted-array.cpp
+++ LeetcodePractice/LinkedList/Merge-k-sorted-array.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//TC-Nlogn SC - LogN
+
 class Solution {
 public:
     ListNode*convert(priority_queue<int, vector<int>, greater<int>> a){
@@ -32,5 +34,46 @@ public:
         }
         if(minHeap.empty()) return nullptr;
         return convert(minHeap);
+    }
+};
+
+//More optimised to O(Nlogk) tc and sc to logk
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct cmp{
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> minHeap;
+        for(auto node : lists){
+            if(node){
+                minHeap.push(node);
+            }
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while(!minHeap.empty()){
+            ListNode* smallest = minHeap.top();
+            minHeap.pop();
+            temp->next = smallest;
+            temp = temp->next;
+            if(smallest->next){
+                minHeap.push(smallest->next);
+            }
+        }
+        return dummy->next;
     }
 };
