353d8432e9ad3467c9e31edaa1b6e1ec15de0d22
Mrunmay <mrunmayurkude20@gmail.com>
2026-05-19 14:17:33 +0530

    first commit

---
+++ basicahh/basics.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n=cardPoints.size()-1;
+        int l=0,r=k-1;
+        int count =0;
+        while(r<=n){
+            co0
+        }
+        return count;
+    }
+};
+
+int main() {
+    vector<int> cardPoints = {6, 2, 3, 4, 7, 2, 1, 7, 1};
+    int k = 4;
+
+    Solution obj;
+    cout << obj.maxScore(cardPoints, k);
+
+    return 0;
+}
